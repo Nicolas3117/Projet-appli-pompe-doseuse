1f741f085db26efb878d76c99c676f1a5affd924
Nicolas3117 <nicolasj.regnier@gmail.com>
2026-02-11 22:05:14 +0100

    modif programmation aider 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.esp32pumpwifi.app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
