1dd3da761d5612804acadf10d9177362df28b0df
Nicolas3117 <nicolasj.regnier@gmail.com>
2026-02-19 09:44:37 +0100

    modif notif même en veille 3

--- app/src/main/java/com/esp32pumpwifi/app/MainActivity.kt
+++ app/src/main/java/com/esp32pumpwifi/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.esp32pumpwifi.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
